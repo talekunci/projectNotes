d3a4c99c61ad849393d2e3548241f2113ab4e98e
Vaka-vaka <Waka2006@ukr.net>
2022-02-16 14:12:05 +0200

    Create users

---
+++ src/main/java/ua/goit/notes/users/UserDetailService.java
@@ -0,0 +1,23 @@
+package ua.goit.notes.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
+        if(userRepository.findByUsername(username).isPresent()) {
+            return userRepository.findByUsername(username).get();
+        }
+        return Optional.of(userRepository.findByUsername(username).get()).get();
+    }
+}
---
+++ src/main/java/ua/goit/notes/users/UserService.java
@@ -0,0 +1,9 @@
+package ua.goit.notes.users;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService extends UserRepository {
+
+
+}
