16fca6f87884b3c5692f22730fdfd613df34ab18
talekunci <smouke.king1@gmail.com>
2022-02-19 23:52:17 +0200

    Fixed LoginController annotation

--- src/main/java/ua/goit/notes/users/LoginController.java
+++ src/main/java/ua/goit/notes/users/LoginController.java
@@ -2,17 +2,16 @@ package ua.goit.notes.users;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.WebAttributes;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
+@RestController
 public class LoginController {
 
     @GetMapping("/login-error")
